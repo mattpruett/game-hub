0b096b545549a2d7573d28cbe84765765580955b
mattpruett <matthewjpruett@gmail.com>
2023-07-18 17:37:29 -0500

    Fetch the games

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -3,7 +3,7 @@ import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
-    <HStack justify={"space-between"}>
+    <HStack>
       <Switch
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
